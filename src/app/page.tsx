e5ee949d84dd2844e4340b845e9c358c17822c2e
Dhanuka Perera <dhanukashehanperera@gmail.com>
2024-04-28 22:37:42 +0100

    create project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800 text-white">
+      <h1 className="text-5xl font-bold mb-4">Coming Soon</h1>
+      <p className="text-xl mb-8">Dhanuka&apos;s Photo Blog</p>
+      <p className="text-lg mt-4">Thank you for your patience!</p>
+    </main>
+  );
+}
